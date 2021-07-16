8756b71164662e2142a092cdb74aac1851888996
ReAl_Ix01 <76906722+realix01@users.noreply.github.com>
2021-07-16 19:07:03 +0545

    Update reconi.sh

--- reconi.sh
+++ reconi.sh
@@ -1,33 +1,77 @@
 #!/bin/bash
-#Author: Piyush Acharya(ReAl_I)
-#Date: 2021/07/16
-#Usage: This is a small script to run some of my goto tools for reconnaisance.
+# Author: Piyush Acharya(ReAl_I)
+# Date: 2021/07/16
+# Usage: This is a small script to run some of my goto tools for reconnaisance.
 
 apt install figlet > /dev/null 2>&1
 figlet -c reconi
 echo "                                                          @Author: Piyush Acharya"
 echo "                                                        https://github.com/realix01"
+echo "Usage: reconi.sh <options>"
+echo "Options: -amass -gau -waybackurls -nuclei -nucleiurls -naabu -all"
 echo
-echo "Usage: reconi.sh <options> all "
-echo "Options: amass gau waybackurls nuclei nucleiurls naabu'
-read -p "Enter the domain: " UserInput
-domain=$UserInput
+read -p "Enter the domain: " domain
 case "${1}" in
-amass)
-amass enum -active -d $domain -o domains.txt | /opt/notify 
+-amass)
+echo "[+] Running amass for subdomain enumeration."
+echo
+amass enum -active -d $domain -o domains.txt
 ;;
-gau)
-cat domains.txt | getallurls > urls.txt | /opt/notify
+-subfinder)
+echo
+echo "[+] Running subfinder on the target domain."
+subfinder -d $domain -all -v
 ;;
-waybackurls)
-cat domains.txt | waybackurls >> urls.txt | cat urls.txt | /opt/notify
+-gau)
+echo
+echo "[+] Running gau on the target domain."
+getallurls -subs > urls.txt
 ;;
-nuclei)
-/opt/nuclei -t ~/nuclei-templates -l domains.txt | /opt/notify
+-waybackurls)
+echo
+echo "[+] Running waybackurls on the target domain."
+/opt/waybackurls -v >> urls.txt | cat urls.txt
 ;;
-nucleiurls)
-/opt/nuclei -t ~/nuclei-templates -l urls.txt | /opt/notify
+-nuclei)
+echo
+echo "[+] Running nuclei on the target domain."
+/opt/nuclei -t ~/nuclei-templates -u $domain
 ;;
-naabu)
-/opt/naabu -iL domains.txt | /opt/notify
+-nucleiurls)
+echo
+echo "[+] Running nuclei on the given urls."
+/opt/nuclei -t ~/nuclei-templates -l urls.txt
+;;
+-naabu)
+echo
+echo "[+] Running naabu on the list of domains."
+/opt/naabu -p -host $domain
+;;
+-all)
+echo
+echo "[+] Running every tools on the target domain"
+echo
+echo "[+] Running amass for subdomain enumeration."
+amass enum -active -d $domain -v -nolocaldb -o domains.txt
+echo
+echo "[+] Running subfinder on the target domain."
+subfinder -d $domain -all -v >> domains.txt
+echo "[+] Sorting unique sub-domains"
+cat domains.txt | sort -u | uniq > domains_sorted.txt | cat domains_sorted.txt
+echo
+echo "[+] Running gau on the target domain." 
+cat domains_sorted.txt | getallurls > urls.txt
+echo
+echo "[+] Running waybackurls on the target domain."
+cat domains_sorted.txt | /opt/waybackurls -v >> urls.txt | cat urls.txt
+echo
+echo "[+] Running nuclei on the target domain."
+/opt/nuclei -t ~/nuclei-templates -l domains_sorted.txt
+echo
+echo "[+] Running nuclei on the given urls."
+/opt/nuclei -t ~/nuclei-templates -l urls.txt
+echo
+echo "[+] Running naabu on the list of domains."
+/opt/naabu -iL -p domains_sorted.txt
+;;
 esac
